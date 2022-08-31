6407fa9a195a685f8aba53dbe859f23ed4e0f8e2
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-08-31 17:12:53 +0000

    buildable

--- src/stargate/stargateConfiguration.ts
+++ src/stargate/stargateConfiguration.ts
@@ -1,4 +1,8 @@
-class StargateConfiguration {
-    public readonly rotorConfigurations: Array<RotorConfiguration>;
+import RotorConfiguration from "./rotorConfiguration";
 
+export default class StargateConfiguration {
+    public readonly rotorConfigurations: Array<RotorConfiguration>;
+    public constructor(rotorConfigurations: Array<RotorConfiguration>) {
+        this.rotorConfigurations = rotorConfigurations;
+    }
 }
