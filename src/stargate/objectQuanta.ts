92fb33eefbb8005be503f4b4f34323a3541a237f
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2022-08-31 19:15:46 +0000

    add complex numbers

--- src/stargate/objectQuanta.ts
+++ src/stargate/objectQuanta.ts
@@ -1,12 +1,15 @@
 import { randomUUID } from "crypto";
 import QuantizedObject from "./quantizedObject";
-
+import { complex, Complex } from "mathjs";
 export default class ObjectQuanta {
     public readonly id: string;
     public readonly associatedObject: QuantizedObject;
     public readonly sequence: bigint;
     public readonly checksum: string;
     public readonly creationDate: Date;
+    public readonly frequency: number;
+    public readonly emState: Complex;
+    public readonly spin: number;
     public constructor(associatedObject: QuantizedObject, sequence: bigint, checksum: string) {
         const date = new Date();
         this.id = randomUUID();
@@ -15,5 +18,8 @@ export default class ObjectQuanta {
         this.checksum = checksum;
         this.creationDate = date;
         // TODO: add quanta physical properties (eg spin, frequency, etc)
+        this.frequency = 0;
+        this.emState = complex(0, 0);
+        this.spin = 0; // negative spin?
     }
 }
